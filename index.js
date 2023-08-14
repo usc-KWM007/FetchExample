923dfab1d1d892d237b594a7e63526b10e3382b1
tgifford-usc <140787784+tgifford-usc@users.noreply.github.com>
2023-08-14 22:15:48 +1000

    Added code files

---
+++ index.js
@@ -0,0 +1,44 @@
+// make variables for the html elements
+const fetchButton = document.getElementById("fetchButton");
+const urlTextBox = document.getElementById("urlTextBox");
+const dataResultArea = document.getElementById("mainResults");
+
+// Make a GET api request
+async function getRequest(someURL) {
+    // clear out the data printout area of the html page
+    dataResultArea.innerHTML = "";
+    
+    // Fetch a response from the REST API
+    const response = await fetch(someURL);
+    
+    // extract the JSON payload from the response
+    const result = await response.json();
+    
+    // Sometimes the payload is actually a string instead of an object. If so convert
+    // it to an object
+    let objResult = (typeof result == "object") ? result  : JSON.parse(result);
+    
+    // and either way, convert the object to a string to print out. 
+    let strResult = JSON.stringify(objResult, undefined, 2);
+    dataResultArea.innerHTML = `<pre><code>${strResult}</code></pre>`;
+    
+    // return the JSON object
+    return objResult;
+}
+
+
+// Make the GET request when the fetch button is clicked
+fetchButton.addEventListener('click', async (event) => {
+    // fetch whatever URL has been typed into textbox
+    let url = urlTextBox.value;
+    let data = await getRequest(url);
+    // Do something else with 'data' if you want
+});
+
+
+// Also fetch the URL if it changes
+urlTextBox.addEventListener('change', async (event) => {
+    let url = urlTextBox.value;
+    let data = await getRequest(url);
+    // Do something else with 'data' if you want
+});
